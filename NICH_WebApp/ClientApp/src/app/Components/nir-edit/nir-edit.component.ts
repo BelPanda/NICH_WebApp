f03d79c40e2c24d0d4aa9ef0c7fa64e5046109d8
panda <panda.judo@outlook.com>
2020-11-20 12:31:19 +0300

    Add nich-edit component

---
+++ NICH_WebApp/ClientApp/src/app/Components/nir-edit/nir-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nir-edit',
+  templateUrl: './nir-edit.component.html',
+  styleUrls: ['./nir-edit.component.css']
+})
+export class NirEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
